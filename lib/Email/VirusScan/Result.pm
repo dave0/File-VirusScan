8b6f40567a28060060bce84f392313036e433b62
Dave O'Neill <dmo@roaringpenguin.com>
2007-05-04 15:03:14 +0000

    Update docs
    r6594@oxygen:  dmo | 2007-05-04 10:32:07 -0400

--- lib/Email/VirusScan/Result.pm
+++ lib/Email/VirusScan/Result.pm
@@ -73,55 +73,66 @@ Email::VirusScan::Result - <One line description of module's purpose>
 =head1 SYNOPSIS
  
     use Email::VirusScan::Result;
-    # Brief but working code example(s) here showing the most common usage(s)
- 
-    # This section will be as far as many users bother reading
-    # so make it as educational and exemplary as possible.
+
+    # It's good
+    return Email::VirusScan::Result->clean();
+
+    # It's bad
+    return Email::VirusScan::Result->virus( 'MyDoom' );
+
+    # It's ugly (er, an error)
+    return Email::VirusScan::Result->error( "Could not execute virus scanner: $!" );
+
+    # And, in the caller....
+    if( $result->is_error() ) {
+	...
+    } elsif ( $result->is_virus() ) {
+	...
+    }
   
 =head1 DESCRIPTION
- 
-A full description of the module and its features.
-May include numerous subsections (i.e. =head2, =head3, etc.) 
- 
-=head1 SUBROUTINES/METHODS 
- 
-A separate section listing the public components of the module's interface. 
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module provides.
-Name the section accordingly.
- 
-In an object-oriented module, this section should begin with a sentence of the 
-form "An object of this class represents...", to give the reader a high-level
-context to help them understand the methods that are subsequently described.
- 
-=head1 DIAGNOSTICS
- 
-A list of every error and warning message that the module can generate
-(even the ones that will "never happen"), with a full explanation of
-each problem, one or more likely causes, and any suggested remedies.
- 
-=head1 CONFIGURATION AND ENVIRONMENT
 
-A full explanation of any configuration system(s) used by the module,
-including the names and locations of any configuration files, and the
-meaning of any environment variables or properties that can be set.
-These descriptions must also include details of any configuration
-language used.
+Encapsulate all return data from a virus scan.  Currently, just holds
+clean/virus/error status, along with a virus name or error message.
  
-=head1 DEPENDENCIES
+=head1 CLASS METHODS 
 
-A list of all the other modules that this module relies upon, including
-any restrictions on versions, and an indication whether these required
-modules are part of the standard Perl distribution, part of the
-module's distribution, or must be installed separately.
+=head2 clean ( )
+
+Create a new object, with no flags set and no data.
+
+=head2 error ( $error_message )
+
+Create a new object with is_error flag set, and data set to
+$error_message.
+
+=head2 virus ( $virusname )
+
+Create a new object with is_virus flag set, and data set to $virusname.
+
+=head2 new ( \%data )
+
+Main constructor.
+
+=head1 INSTANCE METHODS
+
+=head2 is_clean ( )
+
+Returns true if neither is_error nor is_virus was set.
+
+=head2 is_error ( ) 
+
+Returns true if is_error flag was set by constructor.
+
+=head2 is_virus ( ) 
+
+Returns true if is_virus flag was set by constructor.
+
+=head2 get_data ( ) 
+
+Return data value.
 
 =head1 INCOMPATIBILITIES
-
-A list of any modules that this module cannot be used in conjunction
-with.  This may be due to name conflicts in the interface, or
-competition for system or program resources, or due to internal
-limitations of Perl (for example, many modules that use source code
-filters are mutually incompatible).
 
 There are no known incompatibilities with this module.
  
@@ -134,7 +145,6 @@ Patches are welcome.
 =head1 AUTHOR
  
 Dave O'Neill (dmo@roaringpenguin.com)
- 
  
 =head1 LICENCE AND COPYRIGHT
  
