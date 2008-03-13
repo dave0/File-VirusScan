1eaaa81afc5ec3cc6babe1d95595faa094c2140f
Dave O'Neill <dmo@roaringpenguin.com>
2008-03-13 15:57:45 -0400

    Bump version number to 0.010

--- lib/Email/VirusScan.pm
+++ lib/Email/VirusScan.pm
@@ -6,7 +6,7 @@ use Carp;
 use Email::VirusScan::Result;
 use Email::VirusScan::ResultSet;
 
-our $VERSION = '0.002';
+our $VERSION = '0.010';
 
 # We don't use Module::Pluggable.  Most users of this module will have
 # one or two virus scanners, with the other half-dozen or so plugins
